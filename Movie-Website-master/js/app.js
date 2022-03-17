7ffaf055ddd4410448f43008c7d22354fb46b5ab
NoufAlrobiye <Nouf.alrobiye@gmail.com>
2022-03-17 03:44:32 +0300

    edit

--- Movie-Website-master/js/app.js
+++ Movie-Website-master/js/app.js
@@ -1,11 +1,11 @@
-/*
+
  var myModal = document.getElementById('myModal')
  var myInput = document.getElementById('myInput')
 
  myModal.addEventListener('shown.bs.modal', function () {
  myInput.focus()
 })
-*/
+
 
 //get_req
 const axios = require('axios');
